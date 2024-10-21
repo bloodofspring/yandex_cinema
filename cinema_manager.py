8f86ed72d2faa0640e2dc3bab96d6afde71c1364
bloodofspring <t.shagin@gmail.com>
2024-10-21 23:09:10 +0300

    feat: Аргументы разбиты по группам

--- cinema_manager.py
+++ cinema_manager.py
@@ -5,20 +5,25 @@ def main():
     parser = ArgumentParser()
 
     # for operations
+    opertions = parser.add_argument_group('Operations')
+    opertions.add_argument('-o', '--object', help="Объект", type=str)
 
-    parser.add_argument('-o', help="Объект", type=str)
-    parser.add_argument('-c', '--create', help="Создать экземпляр объекта (передавать ничего не надо)")
-    parser.add_argument('-a', '--about', help="Информация об объекте (передавать ничего не надо)")
-    parser.add_argument('-d', '--drop', help="Удалить объект (передавать ничего не надо)")
+    options = parser.add_argument_group('Options')
+    options.add_argument('-c', '--create', help="Создать экземпляр объекта (передавать ничего не надо)")
+    options.add_argument('-a', '--about', help="Информация об объекте (передавать ничего не надо)")
+    options.add_argument('-d', '--drop', help="Удалить объект (передавать ничего не надо)")
 
     # for "customers"
     parser.add_argument('-b', '--buy', help="Купить билет (передавать ничего не надо)")
 
     # for analytics
-    parser.add_argument('-s', '--schedule', help="Расписание фильмов в .docx (передавать ничего не надо)")
-    parser.add_argument('-w', '--workload', help="Загруженность в .xlsx (передавать ничего не надо)")
-    parser.add_argument('-a', '--advertising', help="Реклама в .pptx (передавать ничего не надо)")
+    analytics = parser.add_argument_group("Analytics")
+    analytics.add_argument('-s', '--schedule', help="Расписание фильмов в .docx (передавать ничего не надо)")
+    analytics.add_argument('-wl', '--workload', help="Загруженность в .xlsx (передавать ничего не надо)")
+    analytics.add_argument('-ads', '--advertising', help="Реклама в .pptx (передавать ничего не надо)")
+
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    pass
+    main()
