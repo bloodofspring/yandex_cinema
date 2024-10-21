d7e91b742d4abc638f0d43077442cdd706ef0f76
bloodofspring <t.shagin@gmail.com>
2024-10-21 22:29:56 +0300

    feat: Перенесена модель бд Halls

--- database/models/cinemas.py
+++ database/models/cinemas.py
@@ -1,7 +1,12 @@
-from peewee import CharField
+from peewee import CharField, ForeignKeyField
 
 from database.models.base import BaseModel
 
 
 class Cinemas(BaseModel):
     name = CharField()
+
+
+class Halls(BaseModel):
+    config_json = CharField()
+    cinema = ForeignKeyField(Cinemas, backref="halls")
