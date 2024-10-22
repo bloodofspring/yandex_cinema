5af18fafbdc2a8cce0e0257e686d25ece5d20074
bloodofspring <t.shagin@gmail.com>
2024-10-22 11:37:30 +0300

    feat: Аргументы в классах -Ask приведены к названиям полей из базы данных

---
+++ controllers/ask_functions.py
@@ -0,0 +1,54 @@
+from typing import Any, Self
+from datetime import datetime
+import json
+
+from database.models import Cinemas, Halls
+
+
+class BaseAsk:
+    def __init__(self, data: dict[str, Any]):
+        for k, v in data:
+            self.__dict__[k] = v
+
+    @classmethod
+    def generate(cls) -> Self:
+        data = {}
+
+        return cls(data=data)
+
+    def __call__(self):
+        return self.__dict__
+
+
+class CreateCinemaAsk(BaseAsk):
+    @classmethod
+    def generate(cls) -> Self:
+        data = {}
+        data["name"] = input("Введите название кинотеатра: ")
+
+        return cls(data=data)
+
+
+class CreateHall(BaseAsk):
+    @classmethod
+    def generate(cls) -> Self:
+        data = {}
+        data["cinema"] = Cinemas.delete_by_id(int(input("Введите ID кинотеатра, к которому будет прикреплен зал: ")))
+        n, m = map(int, input("Введите размер зала в формате NxN где N - целое положительное число: ").split("x"))
+        data["config_json"] = json.dumps([[False for _ in range(n)] for _ in range(m)])
+
+        return cls(data=data)
+
+
+class CreateSession(BaseAsk):
+    @classmethod
+    def generate(cls) -> Self:
+        data = {}
+        data["film_name"] = input("Введите название фильма: ")
+        data["starts_at"] = datetime.strptime(
+            input("Введите дату и время начала в формате дд/мм/гггг чч:мм: "), "%d/%m/%Y %HH:%MM"
+        )
+        data["duration"] = int(input("Введите длительность фильма (мин): "))
+        data["hall"] = Halls.get(int(input("Введите ID зала, в котором будет проведен сеанс: ")))
+
+        return cls(data=data)
---
+++ controllers/operation_controller.py
@@ -0,0 +1,26 @@
+from peewee import Model
+from controllers.ask_functions import BaseAsk
+
+
+class OperationController:
+    def __init__(self, cls: Model, obj_id: int | None = None):
+        self.cls: Model = cls
+        self.obj: Model | None = self.get_obj(obj_id ) if obj_id is not None else None
+
+    def get_obj(self, obj_id: int):
+        return self.cls.get_by_id(obj_id)
+
+    def create(self, ask_f: BaseAsk | None = None, **kwargs):
+        if ask_f:
+            kwargs = ask_f.generate()()
+
+        self.obj.create(**kwargs)
+
+    def drop(self):
+        self.obj.delete_by_id(self.obj.ID)
+
+    def set_sample(self):
+        pass
+
+    def config_about(self, sample: str):
+        pass
