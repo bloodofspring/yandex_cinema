a9722bcea4d9c3a839b3df344419a8f122c7cc26
bloodofspring <t.shagin@gmail.com>
2024-10-22 17:23:46 +0300

    feat: Переработаны SessionOC, HallOC, CinemaOC
    (под чем я их писал?..)

--- controllers/operation_controller.py
+++ controllers/operation_controller.py
@@ -28,59 +28,20 @@ class BaseOperationController:
     def drop(self):
         self.type.delete_by_id(self.model.ID)
 
-    def new(self) -> Self:
+    @classmethod
+    def new(cls) -> Self:
         raise NotImplemented("not implemented yet.")
 
     def __str__(self) -> str:
         raise NotImplemented("not implemented yet.")
-
-
-
-
-
-class CinemaOC(BaseOperationController):
-    @classmethod
-    def get(cls, o_id: int | None = None) -> Self:
-        pass
-
-    @classmethod
-    def new(cls):
-        data = {"name": input("Введите название кинотеатра: ")}
-        new = Cinemas.
-        return cls()
-
-def __str__(self) -> str:
-        return (
-            "КИНОТЕАТР {}"
-            "\nЗАЛЫ:"
-            "{}".format(
-            self.name, None
-        ))
-
-
-class HallOC(BaseOperationController):
-    @classmethod
-    def create(cls) -> Self:
-        data = {
-            "cinema": Cinemas.delete_by_id(int(input("Введите ID кинотеатра, к которому будет прикреплен зал: ")))
-        }
-        n, m = map(int, input("Введите размер зала в формате NxN где N - целое положительное число: ").split("x"))
-        data["config_json"] = json.dumps([[False for _ in range(n)] for _ in range(m)])
-
-        return cls(db_cls=Halls, data=data)
-
-    def __str__(self) -> str:
-        return (
-            "ЗАЛ ID={}"
-            "\nБлижайшие сеансы: {}".format(
-                self.db.ID,
-                map(str, map(SessionOC.de_db, Sessions.select().order_by(Sessions.starts_at).desc().limit(3)))
-        ))
 
 
 class SessionOC(BaseOperationController):
+    def __init__(self, model: Model):
+        super().__init__(m_type=Sessions, model=model)
+
     @classmethod
-    def create(cls) -> Self:
+    def new(cls) -> Self:
         data = {
             "film_name": input("Введите название фильма: "),
             "starts_at": datetime.strptime(
@@ -89,12 +50,9 @@ class SessionOC(BaseOperationController):
             "duration": int(input("Введите длительность фильма (мин): ")),
             "hall": Halls.get(int(input("Введите ID зала, в котором будет проведен сеанс: ")))
         }
+        new = Sessions.create(**data)
 
-        return cls(db_cls=Sessions, data=data)
-
-    @classmethod
-    def de_db(cls, o):
-        return cls(db_cls=Sessions, data=o.__dict__)
+        return cls(model=new)
 
     def __str__(self) -> str:
         d = self.__dict__
@@ -107,4 +65,48 @@ class SessionOC(BaseOperationController):
             "\nВремя начала: {}"
             "\nДлительность: {}".format(
                 *d.values()
+        ))
+
+
+class HallOC(BaseOperationController):
+    def __init__(self, model: Model):
+        super().__init__(m_type=Halls, model=model)
+
+    @classmethod
+    def create(cls) -> Self:
+        data = {
+            "cinema": Cinemas.delete_by_id(int(input("Введите ID кинотеатра, к которому будет прикреплен зал: ")))
+        }
+        n, m = map(int, input("Введите размер зала в формате NxN где N - целое положительное число: ").split("x"))
+        data["config_json"] = json.dumps([[False for _ in range(n)] for _ in range(m)])
+        new = Halls.create(**data)
+
+        return cls(model=new)
+
+    def __str__(self) -> str:
+        return (
+            "ЗАЛ ID={}"
+            "\nБлижайшие сеансы: {}".format(
+                self.db.ID,
+                map(str, map(SessionOC.get, map(lambda x: x.ID, Sessions.select().order_by(Sessions.starts_at).desc().limit(3))))
+        ))
+
+
+class CinemaOC(BaseOperationController):
+    def __init__(self, model: Model, **_):
+        super().__init__(m_type=Cinemas, model=model)
+
+    @classmethod
+    def new(cls):
+        data = {"name": input("Введите название кинотеатра: ")}
+        new = Cinemas.create(**data)
+        return cls(model=new)
+
+    def __str__(self) -> str:
+        return (
+            "КИНОТЕАТР {}"
+            "\nЗАЛЫ:"
+            "{}".format(
+            self.model.name,
+                map(str, map(Halls.get, map(lambda x: x.ID, self.model.halls)))
         ))
