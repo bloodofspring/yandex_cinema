6eda6e2d94c64279cf88e56d4f4b0e06ac38ea9e
bloodofspring <t.shagin@gmail.com>
2024-10-24 10:17:06 +0300

    feat: Реализован метод для построения графика купленных билетов

--- controllers/analytics_controller.py
+++ controllers/analytics_controller.py
@@ -1,19 +1,40 @@
 from datetime import datetime
 
 from matplotlib import pyplot
+from numpy import array
+import itertools
 
-from controllers.operation_controller import CinemaOC
+from controllers.operation_controller import CinemaOC, SessionOC
+
+
+class GotData:
+    def __init__(self, cinema: CinemaOC, sessions: list[SessionOC]):
+        self.sessions = sessions
+        self.cinema = cinema
 
 
 class AnalyticsController:
-    def buy_stats(self) -> str:
-        cinema = CinemaOC.get(int(input("Введите ID кинотеатра: ")))
-        pyplot.title(f"Загруженность кинотеатра {cinema.name}")
-        pyplot.ylabel("Количество купленных билетов")
-        pyplot.plot_date(map(lambda x: x, []))
+    @staticmethod
+    def get_data() -> GotData:
+        cin = CinemaOC.get(int(input("Введите ID кинотеатра для построения аналитики: ")))
+        sessions = list(itertools.chain(map(lambda x: x.sessions, cin.model.halls)))
+        return GotData(cinema=cin, sessions=sessions)
 
-        name = f"{cinema.name}_analytics_{str(datetime.now()).replace(' ', '_')}"
-        pyplot.savefig(f"{cinema.name}")
+    def buy_stats(self) -> str:
+        d = self.get_data()
+        an_data = d.sessions[-50:]
+
+        pyplot.title(f"Загруженность кинотеатра {d.cinema.name}")
+        pyplot.ylabel("Количество купленных билетов")
+        pyplot.xlabel(f"День (1: {an_data[0].sold_tickets} -> {len(an_data)}: {an_data[-1].sold_tickets})")
+        pyplot.minorticks_on()
+        pyplot.grid(visible=True)
+
+        arr = array(an_data)
+        pyplot.plot(arr, color="black")
+
+        name = f"{d.cinema.name}_analytics_{str(datetime.now()).replace(' ', '_').replace('.', '')}"
+        pyplot.savefig(name)
 
         return name  # Вернуть имя фала с графиком
 
