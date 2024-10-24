9d17d2f95e22c093e2675fc8d10331eb7ed562aa
bloodofspring <t.shagin@gmail.com>
2024-10-24 11:49:58 +0300

    feat: Перестроен аналитический класс

--- controllers/analytics_controller.py
+++ controllers/analytics_controller.py
@@ -16,23 +16,35 @@ class GotData:
 
 
 class AnalyticsController:
+    def __init__(self):
+        self.data = self.get_data()
+        self.default_file_name = "{}_analytics_{}".format(
+            self.data.cinema.name, str(datetime.now()).replace(' ', '_').replace('.', '')
+        )
+
     @staticmethod
     def get_data() -> GotData:
         cin = CinemaOC.get(int(input("Введите ID кинотеатра для построения аналитики: ")))
         sessions = list(itertools.chain(map(lambda x: x.sessions, cin.model.halls)))
         return GotData(cinema=cin, sessions=sessions)
 
-    @staticmethod
-    def build_pyplot(title: str, xlabel: str, ylabel: str, data: Iterable[int], file_name: str) -> str:
+    def build_pyplot(self, title: str, data: Iterable[int], file_name: str = "", xlabel: str = "", ylabel: str = "") -> str:
         pyplot.title(title)
-        pyplot.xlabel(xlabel)
-        pyplot.ylabel(ylabel)
+
+        if xlabel:
+            pyplot.xlabel(xlabel)
+
+        if ylabel:
+            pyplot.ylabel(ylabel)
 
         pyplot.minorticks_on()
         pyplot.grid(visible=True)
 
         arr = array(data)
         pyplot.plot(arr, color="black")
+
+        if not file_name:
+            file_name = self.default_file_name
 
         pyplot.savefig(file_name)
 
@@ -41,17 +53,14 @@ class AnalyticsController:
 
     def buy_stats(self) -> str:
         d = self.get_data()
-        an_data = d.sessions[-50:]
+        build_data = d.sessions[-50:]
 
         return self.build_pyplot(
             title=f"Загруженность кинотеатра {d.cinema.name}",
-            xlabel=f"День (1: {an_data[0].sold_tickets} -> {len(an_data)}: {an_data[-1].sold_tickets})",
+            xlabel=f"День (1: {build_data[0].sold_tickets} -> {len(build_data)}: {build_data[-1].sold_tickets})",
             ylabel="Количество купленных билетов",
-            data=map(lambda x: len(SoldTickets.select().where(SoldTickets.session == x)), an_data),
-            file_name=f"{d.cinema.name}_analytics_{str(datetime.now()).replace(' ', '_').replace('.', '')}"
+            data=map(lambda x: len(SoldTickets.select().where(SoldTickets.session == x)), build_data),
         )
 
     def session_stats(self) -> str:
-        d = self.get_data()
-
-        return "" # Вернуть имя фала с презентацией
+        return "None"  # coming soon
